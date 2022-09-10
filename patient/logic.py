e07663461047fb645907cbb74725db2f6b227ceb
mirik070 <75161328+mirik070@users.noreply.github.com>
2022-09-10 15:35:06 +0500

    last visit

--- patient/logic.py
+++ patient/logic.py
@@ -16,14 +16,15 @@ def create_record(post_request, user):
 def create_patient(post_request, user):
     fullname = post_request.get('fullname')
     birthday = post_request.get('birthday')
+    last_visit = post_request.get('last_visit')
     address = post_request.get('address')
     phone = post_request.get('phone')
     doc_type = post_request.get('doc_type')
     doc_series = post_request.get('doc_series')
     doc_number = post_request.get('doc_number')
     image = post_request.get('image')
-    patient = Patient.objects.create(fullname=fullname, birthday=birthday, address=address, phone=phone,
-                                     doc_type=doc_type, doc_series=doc_series, doc_number=doc_number,
+    patient = Patient.objects.create(fullname=fullname, birthday=birthday, last_visit=last_visit, address=address,
+                                     phone=phone, doc_type=doc_type, doc_series=doc_series, doc_number=doc_number,
                                      image=image, registrar=user)
     return dict({'back_url': reverse('patient_detail', kwargs={'pk': patient.pk}),
                  'data': ''})
