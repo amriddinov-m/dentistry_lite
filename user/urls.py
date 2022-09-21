ee0c67adca5898f08cf351f4cbae874a4cc06237
mirik070 <75161328+mirik070@users.noreply.github.com>
2022-09-21 19:33:59 +0500

    bug fix

--- user/urls.py
+++ user/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name="login-view"),
     path(".well-known/pki-validation/148A1FAF6AB3ECC87BFB13A95A6A1BA3.txt",
          TemplateView.as_view(template_name="148A1FAF6AB3ECC87BFB13A95A6A1BA3.txt", content_type="text/plain"),
-         ),
+         name='ssl_fl'),
 
 ]
